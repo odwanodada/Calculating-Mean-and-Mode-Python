48b25befb3599b8143878c477749b39738fac084
odwanodada <odwanodada22@gmail.com>
2020-11-05 10:15:27 +0200

    PieChart Graph

--- Pie Graph.py
+++ Pie Graph.py
@@ -3,12 +3,12 @@ import matplotlib.pyplot as plt
 labels = 'girls','mans'
 sizes = [8, 12]
 colors = ['pink', 'blue']
-explode = (0.1, 0) # explode 1st slice
+explode = (0, 0) # to space the pie inside between girls and mans
 
 # Plot
 plt.pie(sizes, explode=explode, labels=labels, colors=colors,
-autopct='%1.1f%%', shadow=True, startangle=140)
+autopct='%2.0f%%', shadow=True, startangle=140)#Explode have been declare on top with Labels and size
 plt.title("Pie Chart Graph for Girls & Mans")
 
-plt.axis('equal')
+plt.axis()
 plt.show()
